247f1a2b5b964b414df06710b36506a58f71993e
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2015-07-01 02:37:15 -0400

    Line Drawing

---
+++ Line/main.c
@@ -0,0 +1,167 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+#include <X11/Xlib.h>
+
+typedef struct {
+	uint32_t x;
+	uint32_t y;
+} Point;
+
+Point points[4];
+uint32_t width, height;
+
+// Bresenham's algorithm
+void drawLine(void *image, Point p1, Point p2) {
+	uint32_t *data;
+	int32_t x, y;
+	int32_t error, e2;
+	int32_t dx, dy;
+	int32_t sx, sy;
+
+	data = (uint32_t *) image;
+
+	x = p1.x;
+	y = p1.y;
+
+	dx = abs(p2.x - p1.x);
+	dy = abs(p2.y - p1.y);
+	sx = p1.x < p2.x ? 1 : -1;
+	sy = p1.y < p2.y ? 1 : -1;
+
+	error = (dx > dy ? dx : -dy) / 2;
+
+	data[p1.y * width + p1.x] = 0x00000000;
+	data[p2.y * width + p2.x] = 0x00000000;
+
+	while (1) {
+		data[y * width + x] = 0x00000000;
+		if (x == p2.x && y == p2.y) break;
+
+		e2 = error;
+
+		if (e2 > -dx) {
+			error -= dy;
+			x += sx;
+		}
+		if (e2 < dy) {
+			error += dx;
+			y += sy;
+		}
+	}
+}
+
+int main() {
+	Display *dpy;
+	Visual *vi;
+	Colormap cmap;
+	XSetWindowAttributes swa;
+	Atom delete_event;
+	GC gc;
+	XImage image;
+	Window root;
+	Window win;
+	int screen;
+
+	width = 600;
+	height = 600;
+	
+	dpy = XOpenDisplay(NULL);
+
+	root = DefaultRootWindow(dpy);
+	screen = DefaultScreen(dpy);
+	cmap = DefaultColormap(dpy, screen);
+	vi = DefaultVisual(dpy, screen);
+	gc = DefaultGC(dpy, screen);
+
+	swa.background_pixel = 0xBCBCBC;
+	swa.colormap = cmap;
+	swa.event_mask = StructureNotifyMask;
+
+	win = XCreateWindow(dpy, root, 0, 0, width, height, 0, CopyFromParent, InputOutput, CopyFromParent, CWBackPixel | CWColormap | CWEventMask, &swa);
+	//win = XCreateSimpleWindow(dpy, root, 0, 0, 600, 600, 0, 0xBCBCBC);
+	
+	delete_event = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
+	XSetWMProtocols(dpy, win, &delete_event, 1);
+
+	XMapWindow(dpy, win);
+	XStoreName(dpy, win, "Line");
+
+	image.width = width;
+	image.height = height;
+	image.xoffset = 0;
+	image.format = ZPixmap;
+	image.data = NULL;
+	image.byte_order = LSBFirst;
+	image.bitmap_unit = 32;
+	image.bitmap_bit_order = MSBFirst;
+	image.bitmap_pad = 32;
+	image.depth = 24;
+	image.bytes_per_line = 0;
+	image.bits_per_pixel = 32;
+	image.red_mask = vi->red_mask;
+	image.green_mask = vi->green_mask;
+	image.blue_mask = vi->blue_mask;
+	image.obdata = NULL;
+
+	image.data = (char *) malloc(sizeof(char) * width * height * 4);
+
+	if (image.data == NULL) {
+		fprintf(stderr, "Unable to allocate memory\n");
+	}
+
+	XInitImage(&image);
+
+	points[0].x = 50;
+	points[0].y = 550;
+
+	points[1].x = 210;
+	points[1].y = 50;
+
+	points[2].x = 380;
+	points[2].y = 550;
+
+	points[3].x = 550;
+	points[3].y = 50;
+
+	XEvent xev;
+	char loop = 1;
+
+	while (loop) {
+		while (XPending(dpy) > 0) {
+			XNextEvent(dpy, &xev);
+
+			switch (xev.type) {
+				case ClientMessage:
+					if (xev.xclient.data.l[0] == delete_event) {
+						loop = 0;
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		uint32_t *data = (uint32_t *) image.data;
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				*data++ = 0x00FFFFFF;
+			}
+		}
+
+		data = (uint32_t *) image.data;
+
+		drawLine(data, points[0], points[1]);
+		drawLine(data, points[1], points[2]);
+		drawLine(data, points[2], points[3]);
+
+		XPutImage(dpy, win, gc, &image, 0, 0, 0, 0, width, height);
+	}
+
+	free(image.data);
+
+	XDestroyWindow(dpy, win);
+	XCloseDisplay(dpy);
+}

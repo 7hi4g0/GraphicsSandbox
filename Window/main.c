c63c1e32164fc164c82bfd29b9c6add7b29522aa
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2015-07-15 12:43:57 -0400

    Added default main() with window

---
+++ Window/main.c
@@ -0,0 +1,8 @@
+#include <Image/image.h>
+#include <Window/window.h>
+
+char windowName[] = "Example Window";
+char fileName[] = "blank.ppm";
+
+void draw(Image image) {
+}
---
+++ Window/window.h
@@ -0,0 +1,9 @@
+#ifndef	WINDOW_H
+#define WINDOW_H
+
+extern char windowName[];
+extern char fileName[];
+
+extern void draw(Image image);
+
+#endif

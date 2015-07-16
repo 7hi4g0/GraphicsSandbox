75a6d95bd41b56fd31d256c370545222e34fa358
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2015-07-16 12:04:28 -0400

    Refactored Bezier example

--- Bezier/main.c
+++ Bezier/main.c
@@ -4,9 +4,14 @@
 #include <X11/Xlib.h>
 #include <GL/glcorearb.h>
 
+#include <Window/window.h>
 #include <Line/line.h>
 
+char windowName[] = "Bezier";
+char fileName[] = "bezier.ppm";
+
 Point points[4];
+uint32_t width, height;
 
 Point bezier(float t) {
 	Point point;
@@ -29,68 +34,8 @@ Point bezier(float t) {
 	return point;
 }
 
-int main() {
-	Display *dpy;
-	Visual *vi;
-	Colormap cmap;
-	XSetWindowAttributes swa;
-	Atom delete_event;
-	GC gc;
-	XImage image;
-	Window root;
-	Window win;
-	int screen;
-	uint32_t width, height;
 
-	width = 600;
-	height = 600;
-	
-	dpy = XOpenDisplay(NULL);
-
-	root = DefaultRootWindow(dpy);
-	screen = DefaultScreen(dpy);
-	cmap = DefaultColormap(dpy, screen);
-	vi = DefaultVisual(dpy, screen);
-	gc = DefaultGC(dpy, screen);
-
-	swa.background_pixel = 0xBCBCBC;
-	swa.colormap = cmap;
-	swa.event_mask = StructureNotifyMask;
-
-	win = XCreateWindow(dpy, root, 0, 0, width, height, 0, CopyFromParent, InputOutput, CopyFromParent, CWBackPixel | CWColormap | CWEventMask, &swa);
-	//win = XCreateSimpleWindow(dpy, root, 0, 0, 600, 600, 0, 0xBCBCBC);
-	
-	delete_event = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
-	XSetWMProtocols(dpy, win, &delete_event, 1);
-
-	XMapWindow(dpy, win);
-	XStoreName(dpy, win, "Bezier");
-
-	image.width = width;
-	image.height = height;
-	image.xoffset = 0;
-	image.format = ZPixmap;
-	image.data = NULL;
-	image.byte_order = LSBFirst;
-	image.bitmap_unit = 32;
-	image.bitmap_bit_order = MSBFirst;
-	image.bitmap_pad = 32;
-	image.depth = 24;
-	image.bytes_per_line = 0;
-	image.bits_per_pixel = 32;
-	image.red_mask = vi->red_mask;
-	image.green_mask = vi->green_mask;
-	image.blue_mask = vi->blue_mask;
-	image.obdata = NULL;
-
-	image.data = (char *) malloc(sizeof(char) * width * height * 4);
-
-	if (image.data == NULL) {
-		fprintf(stderr, "Unable to allocate memory\n");
-	}
-
-	XInitImage(&image);
-
+void prepare() {
 	points[0].x = 50;
 	points[0].y = 550;
 
@@ -102,57 +47,17 @@ int main() {
 
 	points[3].x = 550;
 	points[3].y = 50;
+}
 
-	Image img;
+void draw(Image image) {
+	float step = 0.01f;
+	Point previous = bezier(0);
 
-	img.data = image.data;
-	img.width = width;
-	img.height = height;
+	for (float t = step; t < 1.0f; t += step) {
+		Point p = bezier(t);
 
-	XEvent xev;
-	char loop = 1;
+		drawLine(image, previous, p);
 
-	while (loop) {
-		while (XPending(dpy) > 0) {
-			XNextEvent(dpy, &xev);
-
-			switch (xev.type) {
-				case ClientMessage:
-					if (xev.xclient.data.l[0] == delete_event) {
-						loop = 0;
-					}
-					break;
-				default:
-					break;
-			}
-		}
-
-		uint32_t *data = (uint32_t *) image.data;
-
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				*data++ = 0x00FFFFFF;
-			}
-		}
-
-		data = (uint32_t *) image.data;
-
-		float step = 0.01f;
-		Point previous = bezier(0);
-
-		for (float t = step; t < 1.0f; t += step) {
-			Point p = bezier(t);
-
-			drawLine(img, previous, p);
-
-			previous = p;
-		}
-
-		XPutImage(dpy, win, gc, &image, 0, 0, 0, 0, width, height);
+		previous = p;
 	}
-
-	free(image.data);
-
-	XDestroyWindow(dpy, win);
-	XCloseDisplay(dpy);
 }

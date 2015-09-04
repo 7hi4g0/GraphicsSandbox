c9984fd61f93b21f42312d663414c91dbdfc77b4
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2015-09-04 02:02:11 -0400

    Added more mouse events

--- Window/window.h
+++ Window/window.h
@@ -4,10 +4,14 @@
 #include <Image/image.h>
 
 #define KEY_RELEASE(name)		void name(XKeyEvent xkey)
-#define BUTTON_RELEASE(name)	void name(XButtonEvent xkey)
+#define BUTTON_RELEASE(name)	void name(XButtonEvent xbutton)
+#define BUTTON_PRESS(name)		void name(XButtonEvent xbutton)
+#define BUTTON_MOTION(name)		void name(XMotionEvent xmotion)
 
 typedef KEY_RELEASE(KeyReleaseFn);
 typedef BUTTON_RELEASE(ButtonReleaseFn);
+typedef BUTTON_PRESS(ButtonPressFn);
+typedef BUTTON_MOTION(ButtonMotionFn);
 
 extern char windowName[];
 extern char fileName[];
@@ -16,6 +20,8 @@ extern uint32_t debug;
 extern uint32_t width, height;
 extern KeyReleaseFn *keyReleaseFn;
 extern ButtonReleaseFn *buttonReleaseFn;
+extern ButtonPressFn *buttonPressFn;
+extern ButtonMotionFn *buttonMotionFn;
 
 extern void prepare(void);
 extern void draw(Image image);

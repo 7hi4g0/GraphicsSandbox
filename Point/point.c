3f0e8d2acded016ec16965f59902e40d1a96af35
Thiago Dourado de Andrade <dourado.andrade@gmail.com>
2023-04-04 01:13:56 -0400

    Point: Fixed point sizing while drawing

--- Point/point.c
+++ Point/point.c
@@ -8,8 +8,8 @@ void drawPoint(Image image, Point point, uint8_t size) {
 	pixels = (Pixel *) image.data;
 
 	// TODO: Handle edge cases, i.e. border of the image
-	for (int y = point.y - size; y < point.y + size; y++) {
-		for (int x = point.x - size; x < point.x + size; x++) {
+	for (int y = point.y - size; y <= point.y + size; y++) {
+		for (int x = point.x - size; x <= point.x + size; x++) {
 			pixels[y * image.width + x].value = 0x00000000;
 		}
 	}

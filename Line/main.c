63299017d4376da8e615d9cbc869489de5720e72
Thiago Dourado de Andrade <dourado.andrade@gmail.com>
2023-04-04 01:17:45 -0400

    Line: Improved interactivity of sample application

--- Line/main.c
+++ Line/main.c
@@ -10,7 +10,9 @@
 #define	MaxPoints	32
 
 KEY_RELEASE(keyRelease);
+BUTTON_PRESS(buttonPress);
 BUTTON_RELEASE(buttonRelease);
+BUTTON_MOTION(buttonMotion);
 
 char windowName[] = "Lines";
 char fileName[] = "lines.ppm";
@@ -22,14 +24,38 @@ DrawLineFn drawLineList[4] = {
 	drawLineAntialiasSlow
 };
 
+char* drawLineNameList[4] = {
+	"drawLine",
+	"drawLineThick",
+	"drawLineAntialias",
+	"drawLineAntialiasSlow"
+};
+
 Point points[MaxPoints];
-uint32_t totalPoints = 4;
-uint32_t thickness = 1;
-uint32_t drawFnIndex = 0;
+uint32_t totalPoints;
+uint32_t thickness;
+uint32_t drawFnIndex;
+uint32_t showPoints;
+uint32_t basePointSize;
+uint32_t highlightPointSize;
+uint32_t highlightedPoint;
+uint32_t selectedPoint;
 DrawLineFn drawLineFn;
 
 void prepare(void) {
+	totalPoints = 4;
+	thickness = 1;
+	drawFnIndex = 0;
+	showPoints = 0;
+	basePointSize = 2;
+	highlightPointSize = 3;
+	highlightedPoint = MaxPoints;
+	selectedPoint = MaxPoints;
 	drawLineFn = drawLineList[drawFnIndex];
+
+	if (debug) {
+		printf("%s\n", drawLineNameList[drawFnIndex]);
+	}
 
 	points[0].x = 50;
 	points[0].y = 550;
@@ -45,6 +71,8 @@ void prepare(void) {
 
 	keyReleaseFn = keyRelease;
 	buttonReleaseFn = buttonRelease;
+	buttonPressFn = buttonPress;
+	buttonMotionFn = buttonMotion;
 }
 
 void keyRelease(KeyboardEvent keyboardEvent) {
@@ -53,30 +81,96 @@ void keyRelease(KeyboardEvent keyboardEvent) {
 			if (thickness < 50) {
 				thickness += 1;
 			}
+			if (basePointSize < 25) {
+				uint8_t highlightIncrement;
+				basePointSize +=1;
+				highlightIncrement = (basePointSize >> 2);
+				highlightPointSize = basePointSize + (highlightIncrement > 0 ? highlightIncrement : 1);
+				if (debug) {
+					printf("point size: %d %d\n", basePointSize, highlightPointSize);
+				}
+			}
 			setLineThickness(thickness);
 			break;
 		case KEYCODE_MINUS:
 			if (thickness > 1) {
 				thickness -= 1;
 			}
+			if (basePointSize > 0) {
+				uint8_t highlightIncrement;
+				basePointSize -=1;
+				highlightIncrement = (basePointSize >> 2);
+				highlightPointSize = basePointSize + (highlightIncrement > 0 ? highlightIncrement : 1);
+				if (debug) {
+					printf("point size: %d %d\n", basePointSize, highlightPointSize);
+				}
+			}
 			setLineThickness(thickness);
 			break;
 		case KEYCODE_A:
-			drawLineFn = drawLineList[++drawFnIndex % LineFnCount];
+			drawFnIndex++;
+			drawFnIndex %= LineFnCount;
+			drawLineFn = drawLineList[drawFnIndex];
+			if (debug) {
+				printf("%s\n", drawLineNameList[drawFnIndex]);
+			}
+			break;
+		case KEYCODE_P:
+			showPoints = !showPoints;
 			break;
 		default:
 			break;
 	}
 }
 
+void buttonMotion(MouseEvent mouseEvent) {
+	if (!showPoints) {
+		return;
+	}
+
+	if (selectedPoint < MaxPoints) {
+		points[selectedPoint] = mouseEvent.location;
+		return;
+	}
+
+	highlightedPoint = MaxPoints;
+	for (uint32_t point = 0; point < totalPoints; point++) {
+		if (inPoint(points[point], mouseEvent.location, highlightPointSize)) {
+			highlightedPoint = point;
+			break;
+		}
+	}
+}
+
+void buttonPress(MouseEvent mouseEvent) {
+	if (!showPoints) {
+		return;
+	}
+
+	if (highlightedPoint < MaxPoints) {
+		selectedPoint = highlightedPoint;
+	}
+}
+
 void buttonRelease(MouseEvent mouseEvent) {
-	if (totalPoints < MaxPoints) {
-		points[totalPoints++] = (Point) {mouseEvent.location.x, mouseEvent.location.y};
+	if (selectedPoint < MaxPoints) {
+		selectedPoint = MaxPoints;
+	} else if (totalPoints < MaxPoints) {
+		points[totalPoints++] = mouseEvent.location;
 	}
 }
 
 void draw(Image image) {
+	uint8_t pointSize;
+	if (showPoints) {
+		pointSize = (0 == highlightedPoint) ? highlightPointSize : basePointSize;
+		drawPoint(image, points[0], pointSize);
+	}
 	for (int point = 1; point < totalPoints; point++) {
+		if (showPoints) {
+			pointSize = (point == highlightedPoint) ? highlightPointSize : basePointSize;
+			drawPoint(image, points[point], pointSize);
+		}
 		drawLineFn(image, points[point - 1], points[point]);
 	}
 }
